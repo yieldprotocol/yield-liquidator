fbe0f57fb93e71db8cd525449cc74548f231ed08
Georgios Konstantopoulos <me@gakonst.com>
2020-08-13 20:54:41 +0300

    feat: update Flashloan contract

--- Flash.sol
+++ Flash.sol
@@ -1,7 +1,12 @@
 pragma solidity >=0.6.10;
 
 interface LiquidationLike {
-    function buy(address from, address to, address liquidated, uint256 daiAmount) external;
+    function buy(
+        address from,
+        address to,
+        address liquidated,
+        uint256 daiAmount
+    ) external returns (uint256);
 }
 
 interface PairLike {
@@ -11,23 +16,33 @@ interface PairLike {
 }
 
 interface ERC20Like {
+    // ERC20 approve
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    // ERC20 transfer
     function transfer(address recipient, uint256 amount) external returns (bool);
+
+    // WETH withdraw
+    function withdraw(uint256 amount) external;
 }
 
 contract Flash {
-    PairLike immutable pair;
     address immutable rewards;
+
     LiquidationLike immutable liquidation;
+    PairLike pair;
+    ERC20Like dai;
+    ERC20Like weth;
 
-    ERC20Like immutable dai;
-    ERC20Like immutable weth;
-
-    constructor(address _pair, address _liquidation) public {
+    constructor(address treasury, address _pair, address _liquidation) public {
         pair = PairLike(_pair);
         liquidation = LiquidationLike(_liquidation);
         rewards = msg.sender;
         dai = ERC20Like(pair.token0());
         weth = ERC20Like(pair.token1());
+
+        // allow the treasury to pull funds
+        dai.approve(treasury, type(uint).max);
     }
 
     // amount0 is always DAI for the DAI-WETH pair
@@ -36,24 +51,32 @@ contract Flash {
         require(amount1 == 0, "NON_ZERO_WETH_RECEIVED");
 
         // get the person being liquidated
-        address user = abi.decode(data, (address));
-        
-        // send the received DAI to the auction and receive WETH (at a discount)
-        uint wethReceived = liquidation.buy(address(this), rewards, user, amount0);
+        (address user, uint minProfitETH) = abi.decode(data, (address, uint));
 
-        // Calculate the amount of WETH required:
-        // (same logic as https://github.com/Uniswap/uniswap-v2-periphery/blob/master/contracts/libraries/UniswapV2Library.sol#L55-L59)
+        // send the received DAI to the auction and receive WETH (at a discount)
+        uint wethReceived = liquidation.buy(address(this), address(this), user, amount0);
+
+        // -> Calculate the amount of WETH required
+        //
+        //          wethReserves * daiAmount * 1000
+        // weth = -----------------------------------
+        //          (daiReserves - daiAmount) * 997
         (uint112 daiReserves, uint112 wethReserves,) = pair.getReserves();
-        uint numerator = daiReserves * amount0 * 1000;
-        uint denominator = (wethReserves - amount0) * 997;
+        uint numerator = wethReserves * amount0 * 1000;
+        uint denominator = (daiReserves - amount0) * 997;
         uint wethAmount = numerator / denominator + 1;
-        require(wethReceived > wethAmount, "NOT_ENOUGH_PROFIT");
+
+        require(wethReceived > wethAmount + minProfitETH, "NOT_ENOUGH_PROFIT");
 
         // pay back the loan
         require(weth.transfer(address(pair), wethAmount), "WETH_PAYBACK_FAILED");
 
         // reap the profit!
-        (bool success,) = sender.call{value: amountReceived - amountRequired}(new bytes(0));
-        assert(success);
+        uint profit = wethReceived - wethAmount;
+        weth.withdraw(profit);
+        (bool success,) = sender.call{value: profit}(new bytes(0));
+        require(success, "COULD NOT WITHDRAW");
     }
+
+    receive() external payable {}
 }
